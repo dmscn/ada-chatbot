b94c7559d54fbf85a6857bd3ec89b84f00576826
dmscn-faeterj <virtual.id.dsc@gmail.com>
2018-04-28 16:22:30 -0300

    update

--- index.js
+++ index.js
@@ -6,4 +6,41 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
-app.listen(5000, () => console.log('Webhook server is listening port 5000'));
+app.listen(process.env.port || 1337, () => console.log('Webhook server is listening'));
+
+app.post('/webhook', (req, res) => {
+
+  let body = req.body;
+
+  if(body.object === 'page') {
+    body.entry.forEach(entry => {
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);      
+    });
+
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
+
+});
+
+app.get('/webhook', (req, res) => {
+  
+  let VERIFY_TOKEN = "ada&faeterj-rio";
+
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challange = req.query['hub.challenge'];
+
+  if(mode && token) {
+    if(mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challange);
+    } else {
+      // 403 Forbidden
+      res.sendStatus(403);
+    }
+  }
+
+})
